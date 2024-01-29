9f1cd447dd12ecb0f2c799b70aa2c51b92de0962
Seiya Tanaka <seiya@cloudflare.com>
2024-01-29 21:52:35 +0900

    console.log

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -23,8 +23,11 @@ function LoginForm() {
             if (response.ok) {
                 console.log('ログイン成功');
                 // レスポンスに基づく追加の処理をここに記述（例：ユーザーダッシュボードへのリダイレクト）
-               window.location.href = 'https://analytics.seiyalife.xyz';
-              console.log(JSON.stringify(response))
+           //    window.location.href = 'https://analytics.seiyalife.xyz';
+
+
+
+              console.log(response)
             } else {
                 console.log('ログイン失敗');
                 // エラーハンドリングをここに記述
